15833051d68fb0e7a38c37646e6e4d723017c7df
endy <endymecy@sina.cn>
2016-12-10 17:32:03 +0800

    UnderSampling

---
+++ src/main/scala/org/enme/sampling/UnderSampling.scala
@@ -0,0 +1,82 @@
+package org.enme.sampling
+
+import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.functions._
+
+/**
+  * Created by endy on 16-12-8.
+  */
+
+trait UnderSamplingParams extends Params{
+  final val threshold = new DoubleParam(this, "threshold", "The threshold whether to  " +
+    "undersampling sample of a class", (x: Double) => x > 1)
+  def setThreshold(value: Double): this.type = set(threshold, value)
+
+  final val dependentColName = new Param[String](this, "dependentColName", "The column that " +
+    "provide label values")
+  def setDependentColName(value: String): this.type = set(dependentColName, value)
+
+  final val withReplacement = new BooleanParam(this, "withReplacement", "")
+  def setWithReplacement(value: Boolean): this.type = set(withReplacement, value)
+
+  final val primaryClass = new DoubleParam(this, "primaryClass", "primary class that to under " +
+    "sampling")
+  def setPrimaryClass(value: Double): this.type = set(primaryClass, value)
+}
+
+
+class UnderSampling(override val uid: String) extends Transformer with UnderSamplingParams{
+
+  def this() = this(Identifiable.randomUID("UnderSampling"))
+  /**
+    * Transforms the input dataset.
+    */
+  override def transform(dataset: Dataset[_]): DataFrame = {
+
+    val labelCountPair = dataset.groupBy($(dependentColName)).count().collect()
+
+    val primaryClassCount = labelCountPair
+      .filter{ case Row(label: Double, count: Long) => label == ${primaryClass}}
+      .map(x => x.get(1)).headOption.getOrElse(-1L).asInstanceOf[Long]
+
+    if (primaryClassCount == -1) throw new Exception("The label is not exist")
+
+    val res = labelCountPair.zipWithIndex.map {
+      case (Row(label: Double, count: Long), index: Int) =>
+       val ratio = count / primaryClassCount.toDouble
+
+       /**
+         * if ratio < threshold, only return samples of this label,
+         * otherwise we sample the data from the samples of this label.
+         *
+         * The desired number of samples is : num = primaryClassCount * threshold
+         * so the fraction of sample method is: num / count = threshold / ratio
+         */
+       val df = if (ratio < ${threshold}) dataset.filter(col($(dependentColName)) === label)
+            else dataset.filter(col($(dependentColName)) === label)
+          .sample(${withReplacement}, ${threshold} / ratio)
+
+       df.toDF()
+    }.reduce(_ union _)
+
+    res
+  }
+
+  override def copy(extra: ParamMap): Transformer = defaultCopy(extra)
+
+  /**
+    * :: DeveloperApi ::
+    *
+    * Check transform validity and derive the output schema from the input schema.
+    *
+    * Typical implementation should first conduct verification on schema change and parameter
+    * validity, including complex parameter interaction checks.
+    */
+  override def transformSchema(schema: StructType): StructType = {
+    schema
+  }
+}
